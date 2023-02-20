609a28747177f0bc13c07d554118c79be72a386a
Aleksandr Shuleshko <shurupyan@mail.ru>
2023-02-20 15:52:05 +0100

    chore: 91 add tag model

--- app/core/models.py
+++ app/core/models.py
@@ -3,6 +3,7 @@ Database models.
 """
 
 from django.conf import settings
+from django.core.checks import Tags
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -57,6 +58,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tags for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
