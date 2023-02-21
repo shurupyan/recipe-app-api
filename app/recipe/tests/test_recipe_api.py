a8cdf0f3ff6f98341002a8b78970ed36936364a9
Aleksandr Shuleshko <shurupyan@mail.ru>
2023-02-21 16:05:45 +0100

    feat: 102 Implement update recipe tags feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -255,7 +255,7 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         new_tag = Tag.objects.get(user=self.user, name='Lunch')
-        self.assertEqual(new_tag, recipe.tags.all())
+        self.assertEqual([new_tag], recipe.tags.all())
 
     def test_update_recipe_assign_tag(self):
         """Test assigning an existing tag when updating a recipe."""
