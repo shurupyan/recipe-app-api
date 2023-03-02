7bcb910b7c2883d7869747bf5c40f64d974fc9a3
Aleksandr Shuleshko <shurupyan@mail.ru>
2023-03-02 15:34:02 +0100

    chore: fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,10 +131,6 @@ MEDIA_URL = '/static/media/'
 STATIC_ROOT = '/vol/web/static'
 MEDIA_ROOT = '/vol/web/media'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/vol/web/static'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
